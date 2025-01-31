09e232c06360c2373c35afa775aa9bd60410eb2a
AllanMaloi <maloisaruni@gmail.com>
2025-01-31 18:57:32 +0300

    Update alx_book_store.sql to include IF NOT EXISTS in CREATE TABLE statements

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,17 +1,17 @@
 -- Create database
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Use the created database
 USE alx_book_store;
 
 -- Create Authors table
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 -- Create Books table
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE Books (
 );
 
 -- Create Customers table
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -29,7 +29,7 @@ CREATE TABLE Customers (
 );
 
 -- Create Orders table
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -37,7 +37,7 @@ CREATE TABLE Orders (
 );
 
 -- Create Order_Details table
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
@@ -45,5 +45,3 @@ CREATE TABLE Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
-SHOW DATABASES;
