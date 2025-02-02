4abefc2e1635564525220018fe73216f4bb5490f
AllanMaloi <maloisaruni@gmail.com>
2025-02-02 17:05:11 +0300

    Update task_3.sql to include SHOW TABLES command

--- task_3.sql
+++ task_3.sql
@@ -1,7 +1,5 @@
 -- Use the specified database
 USE alx_book_store;
 
--- Query to list all tables
-SELECT TABLE_NAME 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_SCHEMA = 'alx_book_store';
+-- List all tables
+SHOW TABLES;
